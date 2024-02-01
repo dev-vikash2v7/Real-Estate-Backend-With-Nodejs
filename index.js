c9b7bb9b05e89f0bb2fe5cb1defd9c4da74fd6b6
vikash verma <vikashvermacom92@gmail.com>
2024-02-01 16:30:34 +0530

    'auth_added'

--- index.js
+++ index.js
@@ -3,11 +3,8 @@ import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
-import Stripe from "stripe";
 
 import userRouter from "./routes/User.routes.js";
-import productRouter from "./routes/Product.routes.js";
-import cartRouter from "./routes/Cart.routes.js";
 
 dotenv.config();
 
@@ -21,7 +18,6 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const stripe = Stripe(process.env.STRIPE_SECRET_KEY)
 
 
 mongoose
@@ -38,34 +34,4 @@ mongoose
   });
 
 
-
-  
-  
-app.post('/create-payment-intent', async (req, res) => {
-  try {
-    const { amount  , currency } = req.body;
-
-    const paymentIntent = await stripe.paymentIntents.create({
-
-        amount,
-       currency ,
-
-       payment_method_types: ['card'],
-       description: 'Payment Request From CloudLumous Pvt, Ltd',
-       receipt_email : 'vikashvermacom92@gmail.com',      
-    });
-
-
-    res.status(200).json({ clientSecret: paymentIntent.client_secret });
-
-  } catch (error) {
-    console.error('Error creating Payment Intent:', error.message);
-    res.status(500).json({ error: 'Error creating Payment Intent : ' + error.message });
-  }
-});
-
-
-
 app.use("/user", userRouter);
-app.use("/product", productRouter);
-app.use("/cart", cartRouter);
--- routes/User.routes.js
+++ routes/User.routes.js
@@ -12,6 +12,4 @@ userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
 userRouter.get("/", getAllUsers);
 
-
 export default userRouter;
-  
